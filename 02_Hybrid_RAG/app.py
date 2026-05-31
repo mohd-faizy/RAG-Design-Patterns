30bd59c8b09323fd92565dd4ba86a4436267691f
mohd-faizy <ifaizymohd@gmail.com>
2026-05-31 14:22:24 +0530

    refactor: centralize .env config to root and support find_dotenv

--- 02_Hybrid_RAG/app.py
+++ 02_Hybrid_RAG/app.py
@@ -1,4 +1,4 @@
-from dotenv import load_dotenv
+from dotenv import find_dotenv, load_dotenv
 
 from src.ingestion import (
     load_and_split_documents,
@@ -7,7 +7,7 @@ from src.ingestion import (
 
 from src.graph import build_graph
 
-load_dotenv()
+load_dotenv(find_dotenv())
 
 
 def main():
